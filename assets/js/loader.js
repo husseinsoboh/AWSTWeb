dc3c26695ca462ea28b4de579262a08bfc4f9a67
Hussein Soboh <hussein.soboh@pin-pay.com>
2021-02-25 11:55:10 +0200

    Dev

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -60,7 +60,7 @@ function loadAmozonServices() {
       elem.name +
       "</a></li>";
     div +=
-      '<div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-4 ' +
+      '<div id="amazonServicesDivId" class="carousel-item col-12 col-sm-6 col-md-4 col-lg-4 ' +
       classActive +
       '"><div class="panel panel-default"><div class="panel-thumbnail cl-effect-15"><a href="#" class="thumb" data-hover="' +
       elem.name +
@@ -88,6 +88,7 @@ function amazonServicesEvents() {
   // interval is in milliseconds. 1000 = 1 second -> so 1000 * 10 = 10 seconds
   $("#amazon-services-carousel").carousel({
     interval: 1000 * scrollDurarion,
+    centerMode: true,
   });
 
   /*
@@ -188,29 +189,6 @@ function loadAmazonAdvertising() {
   document.getElementById("amazon-advertising-list").innerHTML = div;
 }
 
-$("#carouselExample").on("slide.bs.carousel", function (e) {
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 4;
-  var totalItems = $(".carousel-item").length;
-
-  if (idx >= totalItems - (itemsPerSlide - 1)) {
-    var it = itemsPerSlide - (totalItems - idx);
-    for (var i = 0; i < it; i++) {
-      // append slides to end
-      if (e.direction == "left") {
-        $(".carousel-item").eq(i).appendTo(".carousel-inner");
-      } else {
-        $(".carousel-item").eq(0).appendTo(".carousel-inner");
-      }
-    }
-  }
-});
-
-$("#carouselExample").carousel({
-  interval: 2000,
-});
-
 function changeLang(lang) {
   let dir = "ltr";
   if (lang == "en") {
@@ -224,133 +202,31 @@ function changeLang(lang) {
   $("[data-localize]").localize("assets/js/lang/lang", { language: lang });
 }
 
-function loadPortfolio(){
-  //root
-  var mc = new Hammer(document.getElementById("carousel-example-generic"));
-
-  //mouse pointer state
-  var pointerXCoord = 0;
-  //image lefting
-  var imageLeftCord = 0;
-  //carousel width
-  var carouselWidth = $(".carousel-inner").width();
-  //lastmove
-  var lastMove = "";
-
-  $("#carousel-example-generic").carousel(
-    {
-      //interval: false
-    }
-  );
-
-  $(".carousel-control.left").click(function() {
-    snapLeft();
+function loadPortfolio() {
+  $("#portfolio-carousel").carousel({
+    interval: 1000 * scrollDurarion,
   });
-  
-  $(".carousel-control.right").click(function() {
-    snapRight();
+
+  $("#portfolio-prev").click(function () {
+    $("#portfolio-carousel").carousel("prev");
   });
-  
-  //CATCH PANNING EVENTS
-  mc.on("panstart panend panleft panright", function(ev) {
-    //PAUSE THE CAROUSEL
-    $("#carousel-example-generic").carousel("pause");
-  
-    //set next and prev with circular searching
-    var prev = $(".item.active").prev();
-    if (prev[0] === undefined) {
-      prev = $(".carousel-inner").children().last();
-    }
-    prev.addClass("prev");
-  
-    var next = $(".item.active").next();
-    if (next[0] === undefined) {
-      next = $(".carousel-inner").children().first();
-    }
-    next.addClass("next");
-  
-    //if is panstart set position of cursor for calculationg different positions
-    if (ev.type === "panstart") {
-      pointerXCoord = ev.pointers[0].pageX;
-      return 0;
-    }
-  
-    //MOVING
-    if (pointerXCoord !== ev.pointers[0].pageX) {
-      //set last action [left-right]
-      lastMove = ev.type;
-  
-      //how much do you move?
-      var diff = ev.pointers[0].pageX - pointerXCoord;
-      $(".item.active").css({
-        transition: "initial",
-        transform: "translate3d(" + (imageLeftCord + diff) + "px, 0, 0)"
-      });
-      $(".item.next").css({
-        transition: "initial",
-        transform:
-          "translate3d(" + (imageLeftCord + diff + carouselWidth) + "px, 0, 0)"
-      });
-      $(".item.prev").css({
-        transition: "initial",
-        transform:
-          "translate3d(" + (imageLeftCord + diff - carouselWidth) + "px, 0, 0)"
-      });
-  
-      //set variables for next turn
-      imageLeftCord += diff;
-      pointerXCoord = ev.pointers[0].pageX;
-    }
-  
-    //end
-    if (ev.type === "panend") {
-      if (imageLeftCord > carouselWidth / 2) {
-        if (lastMove === "panright") {
-          snapLeft();
-          return 0;
-        }
-      }
-  
-      if (imageLeftCord < -(carouselWidth / 2)) {
-        if (lastMove === "panleft") {
-          snapRight();
-          return 0;
-        }
-      }
-  
-      //return to the start position
-      $(".item").css({ transition: "", transform: "" });
-      initialize();
-    }
+
+  $("#portfolio-next").click(function () {
+    $("#portfolio-carousel").carousel("next");
   });
-}
 
-
-
-function initialize() {
-  $("#carousel-example-generic").carousel("cycle");
-  carouselWidth = $(".carousel-inner").width();
-  imageLeftCord = 0;
-  pointerXCoord = 0;
-  lastMove = "";
-}
-
-function snapLeft() {
-  $(".item").css({ transition: "", transform: "" });
-  $(".item").removeClass("prev");
-  $(".item").removeClass("next");
-  $("#carousel-example-generic").carousel("prev");
-  setTimeout(function() {
-    initialize();
-  }, 600);
-}
-
-function snapRight() {
-  $(".item").css({ transition: "", transform: "" });
-  $(".item").removeClass("prev");
-  $(".item").removeClass("next");
-  $("#carousel-example-generic").carousel("next");
-  setTimeout(function() {
-    initialize();
-  }, 600);
+  $(".carousel").swipe({
+    swipe: function (
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction == "left") $(this).carousel("next");
+      if (direction == "right") $(this).carousel("prev");
+    },
+    allowPageScroll: "vertical",
+  });
 }

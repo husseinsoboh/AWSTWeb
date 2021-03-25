3e30b21ebc486b20632183b9958f9b4650f54e7a
Hussein <sobohhussein@hotmail.com>
2021-03-25 14:00:32 +0200

    Dev

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -53,6 +53,358 @@ let contactList = [
   },
 ];
 
+contactList = [
+  {
+    latitude: 23.8859,
+    longitude: 45.0792,
+    imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/SA.svg",
+    label: "Saudi Arabia",
+    TEL: "+966 11 431 1601",
+    MOBILE: "+966 561141047",
+    FAX: "+966 11 291 3929",
+    EMAIL: "INFO@AWTS-LTD.COM",
+    TOLL: "92000 2282",
+    POBOX: "68525 RIYADH 44537",
+  },
+  {
+  latitude: -33.865143,
+  longitude: 151.209900,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/AU.svg",
+  label: "Australia",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 36.2048,
+  longitude: 138.2529,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/JP.svg",
+  label: "Japan",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 35.9078,
+  longitude: 127.7669,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/KR.svg",
+  label: "South Korea",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 35.8617,
+  longitude: 104.1954,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/CN.svg",
+  label: "China",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 22.3193,
+  longitude: 114.1694,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/HK.svg",
+  label: "Hong Kong",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 15.8700,
+  longitude: 100.9925,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/TH.svg",
+  label: "Thailand",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 1.3521,
+  longitude: 103.8198,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/SG.svg",
+  label: "Singapore",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 20.5937,
+  longitude: 78.9629,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/IN.svg",
+  label: "India",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 23.4241,
+  longitude: 53.8478,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/AE.svg",
+  label: "UAE",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 38.9637,
+  longitude: 35.2433,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/TR.svg",
+  label: "Turkey",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 26.8206,
+  longitude: 30.8025,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/EG.svg",
+  label: "Egypt",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 33.8938,
+  longitude: 35.5018,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/LB.svg",
+  label: "LEBANON",
+  TEL: "+961 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.lb",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 31.9539,
+  longitude: 35.9106,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/JO.svg",
+  label: "Jordan",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 40.4637,
+  longitude: 3.7492,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/ES.svg",
+  label: "Spain",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 53.1424,
+  longitude: 7.6921,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/IE.svg",
+  label: "Ireland",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 51.9194,
+  longitude: 19.1451,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/PL.svg",
+  label: "Poland",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 51.509865,
+  longitude: -0.118092,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg",
+  label: "UK",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 46.2276,
+  longitude: 2.2137,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/FR.svg",
+  label: "France",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 52.1326,
+  longitude: 5.2913,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/NL.svg",
+  label: "Netherlands",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 46.8182,
+  longitude: 8.2275,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/CH.svg",
+  label: "Switzerland",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 51.1657,
+  longitude: 10.4515,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/DE.svg",
+  label: "Germany",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 47.5162,
+  longitude: 14.5501,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/AT.svg",
+  label: "Austria",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 41.8719,
+  longitude: 12.5674,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/IT.svg",
+  label: "Italy",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 60.1282,
+  longitude: 18.6435,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg",
+  label: "Sweden",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 37.6000,
+  longitude: -95.6650,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg",
+  label: "USA",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 53.2257,
+  longitude: -91.4062,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/CA.svg",
+  label: "Canada",
+  TEL: "+1 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.ca",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: 19.432608,
+  longitude: -99.133209,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/MX.svg",
+  label: "Mexico",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jo",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+},
+{
+  latitude: -15.793889,
+  longitude: -47.882778,
+  imageURL: "https://purecatamphetamine.github.io/country-flag-icons/3x2/BR.svg",
+  label: "Brazil",
+  TEL: "+962 1 111111",
+  MOBILE: "+961 2 222222",
+  FAX: "+961 3 333333",
+  EMAIL: "awts@email.jod",
+  TOLL: "+961 4 444444",
+  POBOX: "ABC123",
+}
+
+];
+
 function loadMap() {
   am4core.ready(function () {
     loadCountry(contactList[0]);
@@ -94,6 +446,44 @@ function loadMap() {
     imageTemplate.propertyFields.longitude = "longitude";
     imageTemplate.propertyFields.latitude = "latitude";
 
+    let image = imageTemplate.createChild(am4core.Image);
+    image.propertyFields.href = "imageURL";
+    image.userClassName = "labelGrowUp";
+    image.width = 14;
+    image.height = 14;
+    image.horizontalCenter = "middle";
+    image.verticalCenter = "middle";
+
+    /*
+    let label = imageTemplate.createChild(am4core.Label);
+    label.userClassName = "labelGrowUp";
+    label.text = "{label}";
+    label.horizontalCenter = "middle";
+    label.verticalCenter = "top";
+    label.dy = 20;
+
+    label.fontSize = 12;
+    label.fontWeight = "bold";
+    label.verticalCenter = "middle";
+    label.fill = am4core.color("black");
+    */
+
+    image.events.on("hit", function (ev) {
+      console.log(ev.target);
+      console.log(ev.target.properties);
+      console.log(ev.target.properties.href);
+      let country = ev.target.properties.href;//ev.target.currentText;
+      console.log(country);
+      for (let i = 0; i < contactList.length; i++) {
+        let contact = contactList[i];
+        if (contact.imageURL == country) {
+          loadCountry(contact);
+        }
+      }
+    });
+
+
+    /*
     // Creating a pin bullet
     var pin = imageTemplate.createChild(am4plugins_bullets.PinBullet);
 
@@ -145,6 +535,7 @@ function loadMap() {
       max: 5,
       dataField: "value",
     });
+    */
 
     // Pin data
     imageSeries.data = contactList;
@@ -167,80 +558,12 @@ function loadMap() {
     button.icon.path =
       "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
 
-    /*
-    let country_list = getCountryList(); //sessionStorage.getItem("country_list");
-    console.log("country_list");
-    console.log(country_list);
-    //console.log(JSON.parse(country_list));
-
-    AmCharts.makeChart("map", {
-      type: "map",
-      pathToImages: "http://www.amcharts.com/lib/3/images/",
-      addClassNames: true,
-      fontSize: 15,
-      color: "#000000",
-      projection: "miller",
-      backgroundAlpha: 1,
-      backgroundColor: "rgba(163,163,163,1)",
-      dataProvider: {
-        map: "worldHigh",
-        getAreasFromMap: false,
-        images: country_list,
-      },
-      balloon: {
-        horizontalPadding: 15,
-        borderAlpha: 0,
-        borderThickness: 1,
-        verticalPadding: 15,
-      },
-      areasSettings: {
-        color: "rgba(87,87,87,1)",
-        outlineColor: "rgba(163,163,163,1)",
-        rollOverOutlineColor: "rgba(163,163,163,1)",
-        rollOverBrightness: 20,
-        selectedBrightness: 20,
-        selectable: true,
-        unlistedAreasAlpha: 0,
-        unlistedAreasOutlineAlpha: 0,
-        alpha: 0,
-        outlineAlpha: 0,
-      },
-      imagesSettings: {
-        alpha: 1,
-        color: "rgba(87,87,87,1)",
-        outlineAlpha: 0,
-        rollOverOutlineAlpha: 0,
-        outlineColor: "rgba(163,163,163,1)",
-        rollOverBrightness: 20,
-        selectedBrightness: 20,
-        selectable: true,
-      },
-      linesSettings: {
-        color: "rgba(87,87,87,1)",
-        selectable: true,
-        rollOverBrightness: 20,
-        selectedBrightness: 20,
-      },
-      zoomControl: {
-        zoomControlEnabled: true,
-        homeButtonEnabled: false,
-        panControlEnabled: false,
-        right: 38,
-        bottom: 30,
-        minZoomLevel: 0.25,
-        gridHeight: 100,
-        gridAlpha: 0.1,
-        gridBackgroundAlpha: 0,
-        gridColor: "#FFFFFF",
-        draggerAlpha: 1,
-        buttonCornerRadius: 2,
-      },
-    });
-    */
+   
   });
 }
 
 function loadCountry(data) {
+  document.getElementById("country").innerText = data["label"];
   document.getElementById("tel").innerText = data["TEL"];
   document.getElementById("mobile").innerText = data["MOBILE"];
   document.getElementById("fax").innerText = data["FAX"];
